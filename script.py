25d4816981138eb7f4281de04f91e87beb97225b
Joytide <joytide@tutanota.com>
2020-12-18 15:01:25 +0100

    Added coloration functions

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ class Graph():
 		self.nodes=list(set([x for (x,y) in self.edges]+[y for (x,y) in self.edges]))
 		self.nodes.sort()
 		self.two_color=[0,1]
+		self.three_color=[0,0.5,1]
 
 	def show(self):
 		for node in self.nodes:
@@ -52,7 +53,40 @@ class Graph():
 		uncolored_neighbors=[]
 		while len(uncolored)!=0:
 			print(uncolored,uncolored_neighbors)
-			input()
+			#input()
+			if len(uncolored_neighbors)==0:
+				node=uncolored[0]
+			else:
+				node=uncolored_neighbors[0]
+			print("node",node)
+			node_neighbors=self.neighbors(node)
+			node_neighbors_color=[]
+			for neighbor in node_neighbors:
+				if neighbor in self.color_map:
+					node_neighbors_color.append(self.color_map[neighbor])
+				else:
+					uncolored_neighbors.append(neighbor)
+			uncolored_neighbors=list(set(uncolored_neighbors)) 
+			node_neighbors_color=list(set(node_neighbors_color)) #Avoid duplicates
+			print(node_neighbors,node_neighbors_color)
+			#input()
+			if len(node_neighbors_color)>=2:
+				print("node",node,"had too much colors touching it",node_neighbors_color)
+				return False
+			else:
+				self.setnode_color(node,[x for x in self.two_color if x not in node_neighbors_color][0])
+				print("Colored",node,"in",[x for x in self.two_color if x not in node_neighbors_color][0])
+			uncolored.remove(node)
+			if node in uncolored_neighbors:
+				uncolored_neighbors.remove(node)
+		return True
+	
+	def triple_coloration(self):
+		uncolored=self.nodes.copy()
+		uncolored_neighbors=[]
+		while len(uncolored)!=0:
+			print(uncolored,uncolored_neighbors)
+			#input()
 			if len(uncolored_neighbors)==0:
 				node=uncolored[0]
 			else:
@@ -66,33 +100,27 @@ class Graph():
 					node_neighbors_color.append(self.color_map[neighbor])
 				else:
 					uncolored_neighbors.append(neighbor)
-			uncolored_neighbors=list(set(uncolored_neighbors)) 
+			uncolored_neighbors=list(set(uncolored_neighbors))
 			node_neighbors_color=list(set(node_neighbors_color)) #Avoid duplicates
 			print(node_neighbors,node_neighbors_color)
-			input()
-			if len(node_neighbors_color)>=2:
+			#input()
+			if len(node_neighbors_color)>=3:
 				print("node",node,"had too much colors touching it",node_neighbors_color)
 				return False
-			elif len(node_neighbors_color)==1:
-				self.setnode_color(node,[x for x in self.two_color if x not in node_neighbors_color][0])
-				print("Colored",node,"in",[x for x in self.two_color if x not in node_neighbors_color][0])
-
 			else:
-				self.setnode_color(node,self.two_color[0])
-				print("Colored",node,"in",self.two_color[0])
+				self.setnode_color(node,[x for x in self.three_color if x not in node_neighbors_color][0])
+				print("Colored",node,"in",[x for x in self.three_color if x not in node_neighbors_color][0])
 
 			uncolored.remove(node)
 			if node in uncolored_neighbors:
 				uncolored_neighbors.remove(node)
-
 		return True
-			
 
 
 #a=[('A', 'B'), ('A', 'C'), ('D', 'B'), ('E', 'C'), ('E', 'F'),('B', 'H'), ('B', 'G'), ('B', 'F'), ('C', 'G')]
-a=[('A', 'B'), ('B', 'C'), ('B', 'D'),('A', 'E'),('A', 'F')]
+a=[('A', 'B'), ('B', 'C'), ('B', 'D'),('A', 'E'),('A', 'F'),('C','D')]
 G=Graph(a)
-G.double_coloration()
+G.triple_coloration()
 G.show()
 
 
